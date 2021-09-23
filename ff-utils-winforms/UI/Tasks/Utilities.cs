43a046b72b34e0cf8c8fbdde4ef654926b9b5ab7
Julian Maier <login@nmkd.de>
2021-09-23 10:10:36 +0200

    Stream analyze now only runs for selected streams

--- ff-utils-winforms/UI/Tasks/Utilities.cs
+++ ff-utils-winforms/UI/Tasks/Utilities.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Nmkoder.Data;
+using Nmkoder.Data.Ui;
 using Nmkoder.Extensions;
 using Nmkoder.IO;
 using Nmkoder.Media;
@@ -41,12 +42,21 @@ namespace Nmkoder.UI.Tasks
             Program.mainForm.SetWorking(true);
             Logger.Log("Analyzing streams... This can take a few minutes on slower hard drives.");
 
-            foreach(Stream s in MediaInfo.current.AllStreams)
+            foreach(MediaStreamListEntry entry in Program.mainForm.streamListBox.Items)
             {
+                if (!Program.mainForm.streamListBox.GetItemChecked(Program.mainForm.streamListBox.Items.IndexOf(entry)))
+                    continue;
+
+                Stream s = entry.Stream;
                 FfmpegUtils.StreamSizeInfo info = await FfmpegUtils.GetStreamSizeBytes(MediaInfo.current.TruePath, s.Index);
                 string percent = FormatUtils.RatioInt(info.Bytes, MediaInfo.current.Size).ToString("0.0");
                 string br = info.Kbps > 1 ? FormatUtils.Bitrate(info.Kbps.RoundToInt()) : info.Kbps.ToString("0.0") + " kbps";
                 Logger.Log($"Stream #{s.Index} ({s.Type}) - Bitrate: {br} - Size: {FormatUtils.Bytes(info.Bytes)} ({percent}%)");
+            }
+
+            foreach(Stream s in MediaInfo.current.AllStreams)
+            {
+                
             }
 
             Program.mainForm.SetWorking(false);

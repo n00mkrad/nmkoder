36cbc293bdaa1bbcd94cab34ed4b8402c24216ca
n00mkrad <login@nmkd.de>
2021-10-13 09:43:53 +0200

    Don't load video args if video won't be encoded (copy/strip)

--- ff-utils-winforms/UI/Tasks/QuickConvert.cs
+++ ff-utils-winforms/UI/Tasks/QuickConvert.cs
@@ -32,7 +32,7 @@ namespace Nmkoder.UI.Tasks
                 CodecUtils.SubtitleCodec sCodec = GetCurrentCodecS();
                 bool crf = (QualityMode)Program.mainForm.encQualModeBox.SelectedIndex == QualityMode.Crf;
                 bool twoPass = vCodec.SupportsTwoPass && !crf;
-                Dictionary<string, string> videoArgs = GetVideoArgsFromUi(!crf);
+                Dictionary<string, string> videoArgs = vCodec.DoesNotEncode ? new Dictionary<string, string>() : GetVideoArgsFromUi(!crf);
 
                 string inFiles = TrackList.GetInputFilesString();
                 string outPath = GetOutPath(vCodec);
@@ -48,10 +48,10 @@ namespace Nmkoder.UI.Tasks
                 {
                     CodecArgs codecArgsPass1 = vCodec.GetArgs(videoArgs, Pass.OneOfTwo, TrackList.current);
                     string v1 = codecArgsPass1.Arguments;
-                    string vf1 = await GetVideoFilterArgs(vCodec, codecArgsPass1);
+                    string vf1 = vCodec.DoesNotEncode ? "" : await GetVideoFilterArgs(vCodec, codecArgsPass1);
                     CodecArgs codecArgsPass2 = vCodec.GetArgs(videoArgs, Pass.TwoOfTwo, TrackList.current);
                     string v2 = codecArgsPass2.Arguments;
-                    string vf2 = await GetVideoFilterArgs(vCodec, codecArgsPass2);
+                    string vf2 = vCodec.DoesNotEncode ? "" : await GetVideoFilterArgs(vCodec, codecArgsPass2);
 
                     args = $"{custIn} {inFiles} -map v {v1} {vf1} -f null - && ffmpeg -y -loglevel warning -stats " +
                            $"{custIn} {inFiles} {map} {v2} {vf2} {a} {s} {meta} {custOut} {muxing} {outPath.Wrap()}";
@@ -60,7 +60,7 @@ namespace Nmkoder.UI.Tasks
                 {
                     CodecArgs codecArgs = vCodec.GetArgs(videoArgs, Pass.OneOfOne, TrackList.current);
                     string v = codecArgs.Arguments;
-                    string vf = await GetVideoFilterArgs(vCodec, codecArgs);
+                    string vf = vCodec.DoesNotEncode ? "" : await GetVideoFilterArgs(vCodec, codecArgs);
 
                     args = $"{custIn} {inFiles} {map} {v} {vf} {a} {s} {meta} {custOut} {muxing} {outPath.Wrap()}";
                 }

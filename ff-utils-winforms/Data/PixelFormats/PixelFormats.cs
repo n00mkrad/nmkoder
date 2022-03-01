0413c3f78f465f90b9f3e0e0633bf68d1a81ee1d
n00mkrad <login@nmkd.de>
2022-03-01 18:00:39 +0100

    Added missing pix fmt names

--- ff-utils-winforms/Data/PixelFormats/PixelFormats.cs
+++ ff-utils-winforms/Data/PixelFormats/PixelFormats.cs
@@ -15,7 +15,17 @@ namespace Nmkoder.Data.Colors
             string name = "yuv420p";
 
             if (fmt == PF.Yuv420P8) name = "yuv420p";
+            else if (fmt == PF.Yuva420P8) name = "yuva420p";
             else if (fmt == PF.Yuv420P10) name = "yuv420p10le";
+            else if (fmt == PF.Yuv422P8) name = "yuv422p10le";
+            else if (fmt == PF.Yuv422P10) name = "yuv422p10le";
+            else if (fmt == PF.Yuv444P8) name = "yuv444p8le";
+            else if (fmt == PF.Yuv444P10) name = "yuv444p10le";
+            else if (fmt == PF.P010) name = "p010le";
+            else if (fmt == PF.P016) name = "p016le";
+            else if (fmt == PF.Rgb24) name = "rgb24";
+            else if (fmt == PF.Rgba) name = "rgba";
+            else name = "NOT_IMPLEMENTED";
 
             string channels = "yuv";
 
